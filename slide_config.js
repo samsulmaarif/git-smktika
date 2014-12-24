a7ea0ce463bd80b5a30cdb96f083f26892dc2d70
Samsul Ma'arif <samsul@samsul.web.id>
2014-12-25 02:40:29 +0700

    Presentation ready :-P
    Signed-off-by: Samsul Ma'arif <samsul@samsul.web.id>

--- slide_config.js
+++ slide_config.js
@@ -1,18 +1,18 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'Title Goes Here<br>Up To Two Lines',
-    subtitle: 'Subtitle Goes Here',
-    //eventInfo: {
-    //  title: 'Google I/O',
-    //  date: '6/x/2013'
-    //},
+    title: 'Pengenalan Git',
+    subtitle: 'Distributed Version Control System',
+    eventInfo: {
+      title: 'Kelas Khusus',
+      date: '25/XII/2014'
+    },
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
     //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
-    favIcon: 'images/google_developers_logo_tiny.png',
+    favIcon: 'images/logo-ti-24x24.png',
     fonts: [
       'Open Sans:regular,semibold,italic,italicsemibold',
       'Source Code Pro'
@@ -22,12 +22,12 @@ var SLIDE_CONFIG = {
 
   // Author information
   presenters: [{
-    name: 'Firstname Lastname',
-    company: 'Job Title<br>Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
+    name: 'Samsul Ma\'arif',
+    company: 'a Dreamer<br>SMK TI Kawunganten',
+    //gplus: 'http://plus.google.com/1234567890',
+    twitter: '@samsulmaarif_',
+    www: 'http://www.samsul.web.id',
+    github: 'http://github.com/samsulmaarif'
   }/*, {
     name: 'Second Name',
     company: 'Job Title, Google',
